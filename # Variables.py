376c136d4468dadbffd0cfc46a46ca116de1c237
mrchughaarav <mr.chughaarav@gmail.com>
2026-02-09 19:22:37 +0000

    Add files via upload

---
+++ # Variables.py
@@ -0,0 +1,12 @@
+# Variables
+x = 6
+name = "Aarav"
+print(x)
+print(name)
+
+codingal_python = "codingal"
+print(codingal_python)
+
+# User input
+name_new = input("Enter your name:")
+print(f"Hello {name_new}! Welcome to {codingal_python}")
