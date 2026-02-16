ec7cb114cddda5fb4a0748ad262c0384f6043447
mrchughaarav <mr.chughaarav@gmail.com>
2026-02-16 19:31:10 +0000

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,26 @@
+# Assining different variables
+name = "Aarav"
+age = 10
+weight = 29.5
+is_student = True
+
+# Printing different variables and their datatypes
+print("Name:", name)
+print("The Datatype for name is:", type(name))
+
+print("Age:", age)
+print("The Datatype for age is:", type(age))
+
+print("Weight:", weight)
+print("The Datatype for weight is:", type(weight))
+
+print("Is student:", is_student)
+print("The Datatype for is student is:", type(is_student))
+
+# Converting variables to different Datatypes
+age = str(age)
+print(age)
+print("The Datatype for age is:", type(age))
+weight = bool(weight)
+print(weight)
+print("The Datatype for weight is:", type(weight))
