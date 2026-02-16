6ed96d07cf87b23e2111fa906a0e2124b5ce00a6
mrchughaarav <mr.chughaarav@gmail.com>
2026-02-16 18:56:56 +0000

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,12 @@
+# Let's check the values of different datatypes
+a = 5
+print("Type for a:", type(a))
+
+b = 2.5
+print("Type for b:", type(b))
+
+c = "coding"
+print("Type for c:", type(c))
+
+d = "True"
+print("Type for d:", type(d))
