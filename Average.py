fba2b980bc6f06d3870ab77b3f8c0f90396c562f
mrchughaarav <mr.chughaarav@gmail.com>
2026-02-19 18:51:40 +0000

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,13 @@
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+# Finding the total of the trees' heights.
+sum = tree1+tree2+tree3+tree4+tree5
+print(f"The total of the trees' heights is{sum} inches.")
+
+# Finding the average of the trees' heights.
+average = sum/5
+print(f"The average of the heights is{average} inches.")
