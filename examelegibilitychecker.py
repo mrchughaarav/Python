3d9f84e5b56d0e8786a176e4b31fadabef8a227d
mrchughaarav <mr.chughaarav@gmail.com>
2026-03-12 18:52:43 +0000

    Add files via upload

---
+++ examelegibilitychecker.py
@@ -0,0 +1,10 @@
+# Take input from the students to see if they can attend the exam or not
+medical_cause = input("Did you have a medical cause? (Y/N): ").strip().upper()
+if medical_cause == "Y":  # Condition 1
+    print("You are allowed")
+else:
+    atten = int(input("Enter the percantage of the student: "))
+    if atten>= 75:
+        print("Allowed")
+    else:
+        print("Not Allowed")
