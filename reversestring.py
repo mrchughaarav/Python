61243bd4f134c78c25bfaf07026676cd8b1cec04
mrchughaarav <mr.chughaarav@gmail.com>
2026-03-16 19:10:32 +0000

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,6 @@
+string = input("Enter your own string:  ")
+string2 = ('')
+for i in string:
+    string2 = i+string2
+print("The orignal string = ", string)
+print("The reversed sting = ", string2)
