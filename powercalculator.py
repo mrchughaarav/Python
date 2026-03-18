a33c0492cff8eaf2178f3416bd84af7fa79b8342
mrchughaarav <mr.chughaarav@gmail.com>
2026-03-18 20:44:25 +0000

    Add files via upload

---
+++ powercalculator.py
@@ -0,0 +1,4 @@
+number_you_want_to_calculate = int(input("Enter the number you want to calculate the power of: "))
+n = int(input("Enter the power you want to calculate: "))
+result = number_you_want_to_calculate ** n
+print(f"The result of {number_you_want_to_calculate} raised to the power of {n} is: {result}")
