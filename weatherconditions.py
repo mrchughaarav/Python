ae363b04b7eee729b1e8aa24ce821fc5ef8061b8
mrchughaarav <mr.chughaarav@gmail.com>
2026-05-04 19:46:59 +0100

    Add files via upload

---
+++ weatherconditions.py
@@ -0,0 +1,7 @@
+def weather_coditions():
+    print("The weather is pleaseant in:", spring)
+    print("The weather is the same in", autumn)
+
+spring = "autumn"
+autumn = spring
+weather_coditions()
