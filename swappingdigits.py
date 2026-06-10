a48280cf2219f6eb14729a529395af5bb253210f
mrchughaarav <mr.chughaarav@gmail.com>
2026-06-10 18:19:53 +0100

    Add files via upload

---
+++ swappingdigits.py
@@ -0,0 +1,13 @@
+# Input three values
+a = input("Enter first value: ")
+b = input("Enter second value: ")
+c = input("Enter third value: ")
+
+# Swapping positions
+a, b, c = c, a, b
+
+# Output
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
