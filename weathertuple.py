69ee3466946f5a644c42ca3599d57d96f8656372
mrchughaarav <mr.chughaarav@gmail.com>
2026-06-11 20:16:04 +0100

    Add files via upload

---
+++ weathertuple.py
@@ -0,0 +1,13 @@
+weather = (1, 0, 0, 1, 1, 0 )
+sunny = 0 
+rainy = 0
+for i in range (len(weather)):
+    if(weather[i]==0):
+        rainy+=1
+    else:
+        sunny+=1
+
+if(sunny>rainy):
+    print("Good weather!")
+else:
+    print("Bad weather!")
