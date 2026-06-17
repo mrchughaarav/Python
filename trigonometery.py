9622fc0f040b3078f2dbb74ce5ea071d7a3435a8
mrchughaarav <mr.chughaarav@gmail.com>
2026-06-17 20:44:58 +0100

    Add files via upload

---
+++ trigonometery.py
@@ -0,0 +1,10 @@
+import math
+
+angle = float(input("Enter the angle: "))
+
+# Convert degrees to radians
+radians = math.radians(angle)
+
+print("sin =", math.sin(radians))
+print("cos =", math.cos(radians))
+print("tan =", math.tan(radians)) 
