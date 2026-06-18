3da4bf5c1562b92e9d1d080dc309d766907d534e
mrchughaarav <mr.chughaarav@gmail.com>
2026-06-18 19:46:58 +0100

    Add files via upload

---
+++ operationonsets.py
@@ -0,0 +1,16 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.0, "Hello", (1, 2, 3)} 
+print (my_set)
+
+my_set = {1, 2, 3, 4, 2, 3}
+print(my_set)
+
+
+num_set = set([0, 1, 3, 4, 5])
+print("Orignal set:")
+print(num_set)
+num_set.pop()
+print("After removing first element from the said set:")
+print(num_set)
